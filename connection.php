fd1428ccf95433160369b13ab8a04a408fcb01f7
José Carlos de Camargo <jccamargo15@gmail.com>
2017-01-17 16:48:16 -0200

    arquivo obsoleto connection.php

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+// este arquivo não é mais utilizado na nova versão
+
+$host = '127.0.0.1';
+$user = 'root';
+$password = '';
+$database = 'data_base_name';
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+?>
