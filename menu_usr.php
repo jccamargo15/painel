e21392f5d0680d2afa76ab3e689ad5d404759d57
José Carlos de Camargo <jccamargo15@gmail.com>
2017-02-14 15:45:52 -0200

    logo menu usuário corrigido

--- menu_usr.php
+++ menu_usr.php
@@ -1,7 +1,7 @@
 <div class="col-sm-3 col-md-2 sidebar">
 	
 	<div class="negative_logo">
-		<img alt="Grupo Sultec" src="negative_logo.png">
+		<img alt="<?php echo $nome_empresa; ?>" src="images/<?php echo $logo_empresa_negativo; ?>.png">
 	</div>
 
 	<ul class="nav nav-sidebar">
